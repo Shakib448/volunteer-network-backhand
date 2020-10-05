8206aa9e42b01898657b1d0d56cc51a8df7a7525
Shakib448 <shakiba448@gmail.com>
2020-10-05 07:23:38 +0600

    DB UPDATED

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
 const PORT = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.qebvf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const volunteerCollection = client
+    .db("volunteer")
+    .collection("volunteerEntry");
+});
 
 app.use(cors());
 app.use(bodyParser.json());
